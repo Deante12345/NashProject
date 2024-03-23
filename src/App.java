594f4538c3f506b7febf0d52353ff550eb948a90
Deante Dohman <120592230+Deante12345@users.noreply.github.com>
2024-03-22 20:51:47 -0400

    cc

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    static void checkString(String input){
+        char[] temp = input.split(' ');
+    }
+
+
+
 }
+
+
+
